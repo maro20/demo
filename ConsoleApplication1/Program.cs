7533aa9e1145bb580fa7b699c1c950b34f89c309
Marek <marek.rohon@gmail.com>
2015-05-16 17:57:57 +0200

    Added sample employees controller - sync version.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using AdventureWorks.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new AdventureWorks2012Entities())
+            {
+
+                var persons = from p in db.People
+                              select p;
+
+
+                foreach (Person p in persons)
+                {
+                    Console.WriteLine(p.LastName);
+                    
+                }
+
+
+                
+            }
+
+        }
+    }
+}
